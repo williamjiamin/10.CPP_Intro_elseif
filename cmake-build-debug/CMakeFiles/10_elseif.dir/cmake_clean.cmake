b238b9ec8627822ac5852d75daff61fbc8531f5e
williamjiamin <lxodgf@163.com>
2020-06-26 22:42:08 +0800

    init commit

---
+++ cmake-build-debug/CMakeFiles/10_elseif.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "10_elseif"
+  "10_elseif.pdb"
+  "CMakeFiles/10_elseif.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10_elseif.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "-------------Auto Trading V0.01____________" << endl;
+    cout << "1. Start our Auto-Trading program" << endl;
+    cout << "2. Run Back Testing" << endl;
+    cout << "3. Run Web Scraper program" << endl;
+    cout << "4. Search off-line Database" << endl;
+    cout << "5. Quit this program " << endl;
+    cout << "-------------Auto Trading V0.01____________" << endl;
+
+    cout << "Please enter your selection (1-5): " << flush;
+
+    int value;
+    cin >> value;
+
+    if (value == 1) {
+        cout << "Auto-Trading program Started!" << endl;
+    } else if (value == 2) {
+        cout << "Back Testing Started !" << endl;
+    } else if (value == 3) {
+        cout << "Web Scraper Started !" << endl;
+    } else if (value == 4) {
+        cout << "Searching off-line Database......" << endl;
+    } else if (value == 5) {
+        cout << "Program quited" << endl;
+    } else {
+        cout << "Invalid Option, You should only input 1-5 . Sorry~" << endl;
+    }
+
+
+    return 0;
+}
